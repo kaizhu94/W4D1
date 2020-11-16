1e9d18ba22d678563947d8420121b3e53d191172
Stephen <syangrea@gmail.com>
2020-11-16 14:09:01 -0500

    Super Computer Class

--- skeleton/lib/super_computer_player.rb
+++ skeleton/lib/super_computer_player.rb
@@ -2,6 +2,15 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    children_arr = node.children 
+    children_arr.each do |child|
+      return child.prev_move_pos if child.winning_node?(mark)
+    end
+    children_arr.each do |child|
+      return child.prev_move_pos if !(child.losing_node?(mark))
+    end
+    raise "None losing nodes"
   end
 end
 
